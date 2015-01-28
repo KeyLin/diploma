2125bd971d5a137fde85bf93dc3616d93a3647c4
killin <cailin@live.cn>
2015-01-28 10:58:19 +0800

    modify

--- deploy_rasp.sh
+++ deploy_rasp.sh
@@ -1,13 +1,22 @@
+set -e
 cd ~/Desktop
-sudo apt-get install -y python2.7-dev
-sudo apt-get install -y python-pip
-sudo apt-get install -y speex
+apt-get install -y python2.7-dev
+apt-get install -y python-pip
+apt-get install -y speex
 pip install pyrex --allow-all-external --allow-unverified pyrex
 pip install pyaudio --allow-all-external --allow-unverified pyaudio
 #ldconfig
-echo "please install pySpeex"
-wget http://downloads.xiph.org/releases/speex/speex-1.2rc2.tar.gz
+echo "installing pySpeex"
+
+myFile1="./speex-1.2rc2.tar.gz"
+myFile2="./pySpeex-0.2.tar.gz"
+
+if [ ! -f "$myFile1"]; then  
+wget http://downloads.xiph.org/releases/speex/speex-1.2rc2.tar.gz  
+fi
+if [ ! -f "$myFile2"]; then  
+wget http://freenet.mcnabhosting.com/python/pySpeex/pySpeex-0.2.tar.gz  
+fi  
 tar -xzvf speex-1.2rc2.tar.gz
-wget http://freenet.mcnabhosting.com/python/pySpeex/pySpeex-0.2.tar.gz
-tar -xzvf pySpeex-0.2.tar.gz ./speex-1.2rc2/
-python ./speex-1.2rc2/pySpeex/setup.py install
+tar -xzvf pySpeex-0.2.tar.gz -C ./speex-1.2rc2/
+python ./speex*/pySpeex*/setup.py install
