6363934d6d741b98cfe3fb401e7c394529c439d7
killin <cailin@live.cn>
2015-01-28 11:40:23 +0800

    hh

--- deploy_rasp.sh
+++ deploy_rasp.sh
@@ -6,23 +6,25 @@ apt-get install -y speex
 pip install pyrex
 
 myFile0="./pa_stable_v19_20140130.tgz"
-if [ ! -f "$myFile0"]; then
+if [ ! -f "$myFile0" ]; then
 wget http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz
 fi
+tar -xzf pa_stable_v19_20140130.tgz portaudio
+{./portaudio/configure&&make clean&&make&&make install}||{ echo "portaudio install error"; exit 1; }
 
 pip install pyaudio
 
 echo "installing pySpeex"
 
 myFile1="./speex-1.2rc2.tar.gz"
-myFile2="./pySpeex-0.2.tar.gz"
-
-if [ ! -f "$myFile1"]; then  
+if [ ! -f "$myFile1" ]; then  
 wget http://downloads.xiph.org/releases/speex/speex-1.2rc2.tar.gz  
 fi
-if [ ! -f "$myFile2"]; then  
+
+myFile2="./pySpeex-0.2.tar.gz"
+if [ ! -f "$myFile2" ]; then  
 wget http://freenet.mcnabhosting.com/python/pySpeex/pySpeex-0.2.tar.gz  
 fi  
-tar -xzvf speex-1.2rc2.tar.gz
-tar -xzvf pySpeex-0.2.tar.gz -C ./speex-1.2rc2/
-python ./speex*/pySpeex*/setup.py install
+tar -xzf speex-1.2rc2.tar.gz speex
+tar -xzf pySpeex-0.2.tar.gz pySpeex -C ./speex/
+python ./speex/pySpeex/setup.py install
