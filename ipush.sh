68a59b87fa459c55295260aaafe0c585d7242a3a
cailin <linkling@qq.com>
2015-03-05 16:03:40 +0800

    new ipush

--- ipush.sh
+++ ipush.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-str="'"
+
 git add --all
 echo $1
 if [ "$1" = "" ]; then
-	git commit -m ${str}$1${str}
-else
 	git commit -m 'default'
+else
+	str="$1"
+	git commit -m "$str"
 fi
 git push
