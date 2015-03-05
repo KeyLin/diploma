065fef4ac93aa1888060e2789a30b816025b0dfa
root <root@raspberrypi.(none)>
2015-03-05 14:34:23 +0800

    default

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 gcc 8speexenc.c -g -lspeex -o 8enc
 gcc 8speexdec.c -g -lspeex -o 8dec
 
-gcc speexenc.c -g -lspeex -o enc
-gcc speexdec.c -g -lspeex -o dec
+gcc speexenc.c -g -lspeex -o 16enc
+gcc speexdec.c -g -lspeex -o 16dec
