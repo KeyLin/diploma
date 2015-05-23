5b34b403e01693f1e42ff2e5005e6c35a6fd9bfa
killin <linklin@outlook.com>
2015-05-23 21:35:57 +0800

    default

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ asound = cdll.LoadLibrary('libasound.so')
 asound.snd_lib_error_set_handler(c_error_handler)
 
 
-CHUNK = 1024
+CHUNK = 512
 RATE = 16000
 RECORD_SECONDS = 8
 RECORD_CONTROL = int(RATE / CHUNK * RECORD_SECONDS)
