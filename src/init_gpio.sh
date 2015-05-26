12d003b68f44313bb52212a424c9058bad67cfa5
killin <linklin@outlook.com>
2015-05-26 22:55:02 +0800

    default

---
+++ src/init_gpio.sh
@@ -0,0 +1,8 @@
+sudo echo 23 > /sys/class/gpio/export
+sudo echo out > /sys/class/gpio/gpio23/direction
+sudo echo 14 > /sys/class/gpio/export
+sudo echo out > /sys/class/gpio/gpio14/direction
+sudo echo 15 > /sys/class/gpio/export
+sudo echo out > /sys/class/gpio/gpio15/direction
+sudo echo 18 > /sys/class/gpio/export
+sudo echo out > /sys/class/gpio/gpio18/direction
--- src/status.py
+++ src/status.py
@@ -8,46 +8,46 @@ R,G,B=14,15,18
 Anode=23
 
 def pin_init():
-	os.system('sudo echo 23 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio23/direction')
-	os.system('sudo echo 14 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio14/direction')
-	os.system('sudo echo 15 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio15/direction')
-	os.system('sudo echo 18 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio18/direction')
+	os.system('echo 23 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio23/direction')
+	os.system('echo 14 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio14/direction')
+	os.system('echo 15 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio15/direction')
+	os.system('echo 18 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio18/direction')
 
 def reset():
-	os.system('sudo echo 0 > /sys/class/gpio/gpio14/value')
-	os.system('sudo echo 14 > /sys/class/gpio/unexport')
-	os.system('sudo echo 0 > /sys/class/gpio/gpio15/value')
-	os.system('sudo echo 15 > /sys/class/gpio/unexport')
-	os.system('sudo echo 0 > /sys/class/gpio/gpio18/value')
-	os.system('sudo echo 18 > /sys/class/gpio/unexport')
-	os.system('sudo echo 0 > /sys/class/gpio/gpio23/value')
-	os.system('sudo echo 23 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio14/value')
+	os.system('echo 14 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio15/value')
+	os.system('echo 15 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio18/value')
+	os.system('echo 18 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio23/value')
+	os.system('echo 23 > /sys/class/gpio/unexport')
 
 def set_color(color):
 	if color=='red':
-		os.system('sudo echo 1 > /sys/class/gpio/gpio23/value')
-		os.system('sudo echo 0 > /sys/class/gpio/gpio14/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio15/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 0 > /sys/class/gpio/gpio14/value')
+		os.system('echo 1 > /sys/class/gpio/gpio15/value')
+		os.system('echo 1 > /sys/class/gpio/gpio18/value')
 
 	if color=='green':
-		os.system('sudo echo 1 > /sys/class/gpio/gpio23/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio14/value')
-		os.system('sudo echo 0 > /sys/class/gpio/gpio15/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 1 > /sys/class/gpio/gpio14/value')
+		os.system('echo 0 > /sys/class/gpio/gpio15/value')
+		os.system('echo 1 > /sys/class/gpio/gpio18/value')
 
 	if color=='blue':
-		os.system('sudo echo 1 > /sys/class/gpio/gpio23/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio14/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio15/value')
-		os.system('sudo echo 0 > /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 1 > /sys/class/gpio/gpio14/value')
+		os.system('echo 1 > /sys/class/gpio/gpio15/value')
+		os.system('echo 0 > /sys/class/gpio/gpio18/value')
 
 if __name__ == '__main__':
-	pin_init()
+	#pin_init()
 	set_color(color='red')
 	time.sleep(3)
 	set_color(color='green')
