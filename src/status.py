745d30801f0124babd7e944cf1b175746da01ae9
killin <linklin@outlook.com>
2015-05-26 22:30:48 +0800

    default

--- src/status.py
+++ src/status.py
@@ -8,47 +8,43 @@ R,G,B=14,15,18
 Anode=23
 
 def pin_init():
-	os.system('sudo echo 23 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio23/direction')
-	os.system('sudo echo 14 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio14/direction')
-	os.system('sudo echo 15 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio15/direction')
-	os.system('sudo echo 18 > /sys/class/gpio/export')
-	os.system('sudo echo out > /sys/class/gpio/gpio18/direction')
+	os.system('echo 23 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio23/direction')
+	os.system('echo 14 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio14/direction')
+	os.system('echo 15 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio15/direction')
+	os.system('echo 18 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio18/direction')
+
+def reset():
+	os.system('echo 0 > /sys/class/gpio/gpio14/value')
+	os.system('echo 14 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio15/value')
+	os.system('echo 15 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio18/value')
+	os.system('echo 18 > /sys/class/gpio/unexport')
+	os.system('echo 0 > /sys/class/gpio/gpio23/value')
+	os.system('echo 23 > /sys/class/gpio/unexport')
 
 def set_color(color):
 	if color=='red':
-		os.system('sudo echo 1 > /sys/class/gpio/gpio23/value')
-		os.system('sudo echo 0 > /sys/class/gpio/gpio14/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio15/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 0 > /sys/class/gpio/gpio14/value')
+		os.system('echo 1 > /sys/class/gpio/gpio15/value')
+		os.system('echo 1 > /sys/class/gpio/gpio18/value')
 
 	if color=='green':
-		os.system('sudo echo 1 > /sys/class/gpio/gpio23/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio14/value')
-		os.system('sudo echo 0 > /sys/class/gpio/gpio15/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 1 > /sys/class/gpio/gpio14/value')
+		os.system('echo 0 > /sys/class/gpio/gpio15/value')
+		os.system('echo 1 > /sys/class/gpio/gpio18/value')
 
 	if color=='blue':
-		os.system('sudo echo 1 > /sys/class/gpio/gpio23/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio14/value')
-		os.system('sudo echo 1 > /sys/class/gpio/gpio15/value')
-		os.system('sudo echo 0 > /sys/class/gpio/gpio18/value')
-
-	# gpio.set(Anode,1)
-	# gpio.setup(R,'out')
-	# gpio.set(R,1)
-	# gpio.setup(G,'out')
-	# gpio.set(G,1)
-	# gpio.setup(B,'out')
-	# gpio.set(B,1)
-	# if color == 'red':
-	# 	gpio.set(R,0)
-	# elif color == 'green':
-	# 	gpio.set(G,0)
-	# elif color == 'blue':
-	# 	gpio.set(B,0)
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 1 > /sys/class/gpio/gpio14/value')
+		os.system('echo 1 > /sys/class/gpio/gpio15/value')
+		os.system('echo 0 > /sys/class/gpio/gpio18/value')
 
 if __name__ == '__main__':
 	pin_init()
@@ -58,5 +54,5 @@ if __name__ == '__main__':
 	time.sleep(3)
 	set_color(color='blue')
 	time.sleep(3)
-	#reset()
+	reset()
 		
