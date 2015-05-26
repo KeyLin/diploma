a015d3924f49d5c60c92c2b84e715c0d2c6020e7
killin <linklin@outlook.com>
2015-05-26 21:14:48 +0800

    default

--- src/status.py
+++ src/status.py
@@ -7,6 +7,12 @@ import time
 R,G,B=14,15,18
 Anode=23
 
+def reset():
+	gpio.set(Anode,0)
+	gpio.set(R,0)
+	gpio.set(G,0)
+	gpio.set(B,0)
+
 def set_color(color):
 	gpio.setup(Anode,'out')
 	gpio.set(Anode,1)
@@ -25,4 +31,9 @@ def set_color(color):
 
 if __name__ == '__main__':
 	set_color(color='red')
+	time.sleep(3)
+	set_color(color='green')
+	time.sleep(3)
+	set_color(color='blue')
+	reset()
 		
