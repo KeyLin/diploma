30a338ea0170e7eaa1f2982783b56233d5a353bf
killin <linklin@outlook.com>
2015-05-27 01:36:33 +0800

    default

--- src/sphinx.py
+++ src/sphinx.py
@@ -78,7 +78,7 @@ class Pocket(object):
         self.decoder.process_raw(audio_buf, False, False)
         self.window -= 1
         print('Best hypothesis segments: ', [seg.word for seg in self.decoder.seg()])
-        return [seg.word for seg in self.decoder.seg()]
+        self.result = [seg.word for seg in self.decoder.seg()]
         # try:
         #     # If the decoder has partial results, display them in the screen.
         #     if self.decoder.hyp().hypstr != '':
