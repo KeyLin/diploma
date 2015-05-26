c98f42a229b6590b50ae193da551da131a57704b
killin <linklin@outlook.com>
2015-05-26 22:19:36 +0800

    default

--- src/status.py
+++ src/status.py
@@ -8,33 +8,33 @@ R,G,B=14,15,18
 Anode=23
 
 def pin_init():
-	os.system('echo 23 /sys/class/gpio/export')
-	os.system('echo out /sys/class/gpio/gpio23/direction')
-	os.system('echo 14 /sys/class/gpio/export')
-	os.system('echo out /sys/class/gpio/gpio14/direction')
-	os.system('echo 15 /sys/class/gpio/export')
-	os.system('echo out /sys/class/gpio/gpio15/direction')
-	os.system('echo 15 /sys/class/gpio/export')
-	os.system('echo out /sys/class/gpio/gpio18/direction')
+	os.system('echo 23 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio23/direction')
+	os.system('echo 14 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio14/direction')
+	os.system('echo 15 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio15/direction')
+	os.system('echo 15 > /sys/class/gpio/export')
+	os.system('echo out > /sys/class/gpio/gpio18/direction')
 
 def set_color(color):
 	if color=='red':
-		os.system('echo 1 /sys/class/gpio/gpio23/value')
-		os.system('echo 0 /sys/class/gpio/gpio14/value')
-		os.system('echo 1 /sys/class/gpio/gpio15/value')
-		os.system('echo 1 /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 0 > /sys/class/gpio/gpio14/value')
+		os.system('echo 1 > /sys/class/gpio/gpio15/value')
+		os.system('echo 1 > /sys/class/gpio/gpio18/value')
 
 	if color=='green':
-		os.system('echo 1 /sys/class/gpio/gpio23/value')
-		os.system('echo 1 /sys/class/gpio/gpio14/value')
-		os.system('echo 0 /sys/class/gpio/gpio15/value')
-		os.system('echo 1 /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 1 > /sys/class/gpio/gpio14/value')
+		os.system('echo 0 > /sys/class/gpio/gpio15/value')
+		os.system('echo 1 > /sys/class/gpio/gpio18/value')
 
 	if color=='blue':
-		os.system('echo 1 /sys/class/gpio/gpio23/value')
-		os.system('echo 1 /sys/class/gpio/gpio14/value')
-		os.system('echo 1 /sys/class/gpio/gpio15/value')
-		os.system('echo 0 /sys/class/gpio/gpio18/value')
+		os.system('echo 1 > /sys/class/gpio/gpio23/value')
+		os.system('echo 1 > /sys/class/gpio/gpio14/value')
+		os.system('echo 1 > /sys/class/gpio/gpio15/value')
+		os.system('echo 0 > /sys/class/gpio/gpio18/value')
 
 	# gpio.set(Anode,1)
 	# gpio.setup(R,'out')
