aa138c2e5c7861b00b2215ab02c718f56c28b7ba
killin <linklin@outlook.com>
2015-05-26 21:09:49 +0800

    default

--- src/status.py
+++ src/status.py
@@ -7,37 +7,22 @@ import time
 R,G,B=14,15,18
 Anode=23
 
-class RGB(object):
-	"""docstring for RGB"""
-	def __init__(self):
-		super(RGB, self).__init__()
-
-	def reset(anode,red,green,blue):
-		gpio.setup(anode,'out')
-		gpio.set(anode,0)
-		gpio.setup(red,'out')
-		gpio.set(red,0)
-		gpio.setup(green,'out')
-		gpio.set(green,0)
-		gpio.setup(blue,'out')
-		gpio.set(blue,0)
-
-
-	def set_color(color):
-		gpio.setup(Anode,'out')
-		gpio.set(Anode,1)
-		gpio.setup(R,'out')
-		gpio.set(R,1)
-		gpio.setup(G,'out')
-		gpio.set(G,1)
-		gpio.setup(B,'out')
-		gpio.set(B,1)
-		if color == 'red':
-			gpio.set(R,0)
-		elif color == 'green':
-			gpio.set(G,0)
-		elif color == 'blue':
-			gpio.set(B,0)
+def set_color(color):
+	gpio.setup(Anode,'out')
+	gpio.set(Anode,1)
+	gpio.setup(R,'out')
+	gpio.set(R,1)
+	gpio.setup(G,'out')
+	gpio.set(G,1)
+	gpio.setup(B,'out')
+	gpio.set(B,1)
+	if color == 'red':
+		gpio.set(R,0)
+	elif color == 'green':
+		gpio.set(G,0)
+	elif color == 'blue':
+		gpio.set(B,0)
+		
 if __name__ == '__main__':
 	RGB.set_color(color='red')
 		
